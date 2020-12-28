8fac08b0054cb336240e779617a31a07474f0d8d
laxedo17 <saracyan78@hotmail.com>
2020-12-28 22:32:04 +0100

    Add project files.

---
+++ GestionColegioMVC/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="GestionColegioMVC.MvcApplication" Language="C#" %>
---
+++ GestionColegioMVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(GestionColegioMVC.Startup))]
+namespace GestionColegioMVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
